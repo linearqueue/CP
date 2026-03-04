5039da4c2a815d29322bd7ce47f216ae00cd41de
shubhamgaur_ <24204031154@stu.manit.ac.in>
2026-03-04 16:30:53 +0530

    Change minimum to maximum in sliding window implementation

--- Sliding Window/Form 0/2_Minimum_in_K_Window.cpp
+++ Sliding Window/Form 0/2_Minimum_in_K_Window.cpp
@@ -2,36 +2,40 @@
 using namespace std;
 #define endl "\n"
 #define int long long
-int arr[100100];
-int n, k;
 
 // One thing at a time
-// Creating dataStructure
+
 struct monotone_deque {
   deque<int> dq;
 
   void insert(int x) {
-    while (dq.back() > x && !dq.empty()) dq.pop_back();
+    // sirf 1 line change max mil jayega in window
+
+ // while (!dq.empty() && dq.back() < x) dq.pop_back();
+    while (!dq.empty() && dq.back() > x) dq.pop_back();
     dq.push_back(x);
   }
-
   void erase(int x) {
     if (dq.front() == x) dq.pop_front();
   }
 
-  int getmin() { return dq.front(); }
+  int getMax() { return dq.front(); }
 };
-
 void solve() {
-  // same code of previous O(n*log k)
-  // here => O(n)
-  // hamesh yhi uper wala datastructure use krna hai struct bna ke
+  int n, k;
+  cin >> n >> k;
+  int arr[n];
+  for (int i = 0; i < n; i++) cin >> arr[i];
 
   monotone_deque mt;
   for (int i = 0; i < n; ++i) {
     mt.insert(arr[i]);
-    if ((i - k) >= 0) mt.erase(arr[i - k]);         // mt.size()>k
-    if (i >= (k - 1)) cout << mt.getmin() << endl;  // mt.size()==k
+    if ((i - k) >= 0) {
+      mt.erase(arr[i - k]);
+    }
+    if (i >= (k - 1)) {
+      cout << mt.getMax() << " ";
+    }
   }
 }
 
@@ -39,9 +43,6 @@ signed main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-
-  cin >> n >> k;
-  for (int i = 0; i < n; i++) cin >> arr[i];
 
   int t = 1;
   // cin >> t;
